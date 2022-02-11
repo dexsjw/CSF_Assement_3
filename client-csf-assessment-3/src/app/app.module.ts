294d8357603c9b5475833c5f08d3ff6c8bb739e8
Dexter Soh <dextersjw@gmail.com>
2022-02-11 09:38:08 +0800

    push 2

--- client-csf-assessment-3/src/app/app.module.ts
+++ client-csf-assessment-3/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeAddComponent } from './recipe-add/recipe-add.component';
+
+const appRoutes: Routes = [
+  { path: '', component: RecipeListComponent},
+  { path: 'recipe/:recipeId', component: RecipeDetailComponent},
+  { path: 'add', component: RecipeAddComponent},
+  { path: '**', redirectTo: '/', pathMatch: 'full'},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeAddComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
